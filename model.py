46f8e8c4a8af070aa4660aff81b05cad21ecfab3
Sungmin Kim <sungminkim@dynamic-oit-ip4-wifirestricted03-10-49-123-63.princeton.edu>
2025-03-21 15:45:55 -0400

    defined LeNet5

--- model.py
+++ model.py
@@ -33,3 +33,38 @@
     # format tensors into data loaders
     train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
     test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = True)
+
+    # LeNet5 definition: 3 fully-connected layers and 2 convolutional layers
+    class LeNet5(nn.Module):
+        # constructor
+        def __init__(self, num_classes):
+            super(ConvNeuralNet, self).__init__() # inherit from nn.Module class, construct parent
+            # two convolutional layers using ReLU activation function and max pooling
+            self.layer1 = nn.Sequential( 
+                nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+                nn.BatchNorm2d(6),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 2, stride = 2))
+            self.layer2 = nn.Sequential(
+                nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 2, stride = 2))
+            # three fully-connected layers defined
+            self.fc1 = nn.Linear(400, 120)
+            self.relu1 = nn.ReLU()
+            self.fc2 = nn.Linear(120, 84)
+            self.relu2 = nn.ReLU()
+            self.fc3 = nn.Linear(84, num_classes)
+        
+        # sequentially passes data through all of the layers
+        def forward(self, x): # called by default when data passed through model
+            out = self.layer1(x)
+            out = self.layer2(out)
+            out = out.reshape(out.size(0), -1)
+            out = self.fc1(out)
+            out = self.relu1(out)
+            out = self.fc2(out)
+            out = self.relu2(out)
+            out = self.fc3(out)
+            return out
